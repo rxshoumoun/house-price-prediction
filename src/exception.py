48deaba07620cbe83f13fa2f1f27e9d2c6df71d1
rxshoumoun <rohanj3217@gmail.com>
2023-12-19 00:43:20 +0530

    notebook

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,4 @@
 import sys
-import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -17,5 +16,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-
